f9175138bb96c1cf8dda1d20deb68a110ba7f045
Jakub Cendrowski <66736611+Quuba@users.noreply.github.com>
2022-12-04 20:46:32 +0100

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input_data = fs::read_to_string("src/input.txt").expect("couldn't open file");
+
+    let mut max_calories = 0;
+    let mut tmp_sum = 0;
+    for line in input_data.lines() {
+        if line == "" {
+            if tmp_sum > max_calories {
+                max_calories = tmp_sum;
+            }
+            tmp_sum = 0;
+            continue;
+        }
+        tmp_sum += line.parse::<i32>().unwrap();
+    }
+    println!("most calories: {}", max_calories);
 }
