72315ca3e21c07d9660f67719ab33edb16f31b25
Jakub Cendrowski <66736611+Quuba@users.noreply.github.com>
2022-12-04 21:34:09 +0100

    changed method of finding top calories

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,28 @@ use std::fs;
 fn main() {
     let input_data = fs::read_to_string("src/input.txt").expect("couldn't open file");
 
-    let mut max_calories = 0;
+    let mut top_calories: [i32; 3] = [0, 0, 0];
     let mut tmp_sum = 0;
     for line in input_data.lines() {
         if line == "" {
-            if tmp_sum > max_calories {
-                max_calories = tmp_sum;
+            if tmp_sum > top_calories[0] {
+                top_calories.swap(1, 2);
+                top_calories.swap(0, 1);
+                top_calories[0] = tmp_sum;
+            } else if tmp_sum > top_calories[1] && tmp_sum < top_calories[0] {
+                top_calories.swap(1, 2);
+                top_calories[1] = tmp_sum;
+            } else if tmp_sum > top_calories[2] && tmp_sum < top_calories[1] {
+                top_calories[2] = tmp_sum;
             }
             tmp_sum = 0;
             continue;
         }
         tmp_sum += line.parse::<i32>().unwrap();
     }
-    println!("most calories: {}", max_calories);
+    let top_calories_sum = top_calories.iter().sum::<i32>();
+    println!(
+        "top 3 calories: {:#?} \n sum: {}",
+        top_calories, top_calories_sum
+    );
 }
