ee567565c796e761b052149dacc94d7e2698f086
Jakub Cendrowski <66736611+Quuba@users.noreply.github.com>
2022-12-04 22:06:50 +0100

    finished

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ fn main() {
     let mut point_sum = 0;
     for line in input_data.lines() {
         let (a, b) = line.split_at(1);
-        let player_move: Move;
         let opponent_move: Move;
+        let round_result: RoundResult;
         match a.trim() {
             "A" => opponent_move = Move::Rock,
             "B" => opponent_move = Move::Paper,
@@ -14,11 +14,28 @@ fn main() {
             _ => panic!(),
         }
         match b.trim() {
-            "X" => player_move = Move::Rock,
-            "Y" => player_move = Move::Paper,
-            "Z" => player_move = Move::Scissors,
+            "X" => round_result = RoundResult::Loss,
+            "Y" => round_result = RoundResult::Draw,
+            "Z" => round_result = RoundResult::Win,
             _ => panic!(),
         }
+        let player_move: Move;
+        match round_result {
+            RoundResult::Win => match opponent_move {
+                Move::Rock => player_move = Move::Paper,
+                Move::Paper => player_move = Move::Scissors,
+                Move::Scissors => player_move = Move::Rock,
+            },
+            RoundResult::Loss => match opponent_move {
+                Move::Rock => player_move = Move::Scissors,
+                Move::Paper => player_move = Move::Rock,
+                Move::Scissors => player_move = Move::Paper,
+            },
+            RoundResult::Draw => {
+                player_move = opponent_move.clone();
+            }
+        }
+
         point_sum += calculate_points(opponent_move, player_move)
     }
     println!("sum of points: {}", point_sum);
@@ -37,21 +54,25 @@ fn calculate_points(opponent_move: Move, your_move: Move) -> i32 {
     {
         //you've won
         points += 6;
-
     } else if your_move == Move::Rock && opponent_move == Move::Paper
         || your_move == Move::Paper && opponent_move == Move::Scissors
         || your_move == Move::Scissors && opponent_move == Move::Rock
     {
         //opponent won
-    } else if your_move == opponent_move{
+    } else if your_move == opponent_move {
         //draw
         points += 3;
     }
     return points;
 }
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Eq, Clone)]
 enum Move {
     Rock,
     Paper,
     Scissors,
+}
+enum RoundResult {
+    Win,
+    Loss,
+    Draw,
 }
