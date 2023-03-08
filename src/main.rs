fb4050cb6912a105d306f8851e0baef8a833c733
Quuba <jb.cendrowski@gmail.com>
2023-03-08 23:02:35 +0100

    finished

--- src/main.rs
+++ src/main.rs
@@ -5,40 +5,37 @@ fn main() {
     let input_chars = input_data.chars().collect::<Vec<char>>();
     let mut final_val = 0;
 
-    let mut buffer: [char; 4] = [
-        input_chars[0],
-        input_chars[1],
-        input_chars[2],
-        input_chars[3],
-    ];
-    
-    if is_buffer_unique(&buffer){
-        final_val = 4;
+    let mut buffer: [char; 14] = [' '; 14];
+    for i in 0..14 {
+        buffer[i] = input_chars[i];
+    }
+
+    if is_buffer_unique(&buffer) {
+        final_val = 14;
         println!("{final_val}");
         return;
     }
 
-    for i in 4..input_chars.len() {
+    for i in 14..input_chars.len() {
         shift_buffer(&mut buffer, input_chars[i]);
         if is_buffer_unique(&buffer) {
-            final_val = i+1;
+            final_val = i + 1;
             break;
         }
     }
 
     println!("{final_val}");
-
 }
-fn shift_buffer(buffer: &mut [char; 4], new_char: char) {
-    for i in 0..3 {
+fn shift_buffer(buffer: &mut [char; 14], new_char: char) {
+    for i in 0..13 {
         buffer[i] = buffer[i + 1];
     }
-    buffer[3] = new_char;
+    buffer[13] = new_char;
 }
 
-fn is_buffer_unique(buffer: &[char; 4]) -> bool {
-    for i in 0..3 {
-        for j in 0..4 {
+fn is_buffer_unique(buffer: &[char; 14]) -> bool {
+    for i in 0..13 {
+        for j in 0..14 {
             if j == i {
                 continue;
             }
